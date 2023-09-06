3736684311e54c06ee1e5cc13ae054e5e498a1bd
shaahulhamid <shaahulhamid.kassim@gmail.com>
2023-09-06 14:30:40 +0530

    Done!

---
+++ typecast.java
@@ -0,0 +1,26 @@
+class typecast 
+{
+	public static void main(String[] args) 
+	{
+		float a=10.5;
+		
+		/*byte c = a ;
+		System.out.println("byte :"+c);*/
+		
+		/*short e = a;
+		System.out.println("Int :"+e);*/
+
+		/*long d = a ;
+		System.out.println("Long :"+d);*/
+
+		double b = a ;
+		System.out.println("Double :"+b);
+		
+		/*float f = a ;
+		System.out.println("Float :"+f);*/
+		
+		//char g = a ;
+		//System.out.println("Char :"+g);
+	
+	}
+}
